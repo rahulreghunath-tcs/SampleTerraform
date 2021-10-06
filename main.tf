86a0136bdfd8a2a53168ac27682c6efc388e17d8
rahulreghunath-tcs <92011839+rahulreghunath-tcs@users.noreply.github.com>
2021-10-06 18:20:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ data "azurerm_resource_group" "example" {
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "example" {
