4d40ea2aa4e08402a2d63cbf1468f319bb20080a
rahulreghunath-tcs <92011839+rahulreghunath-tcs@users.noreply.github.com>
2021-10-11 18:27:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,51 +1,62 @@
-provider "azurerm" {
-  version = "~>2.0"
-  skip_provider_registration = true
-  features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">=2.80.0"
+      # version = "~>2.80.0" For production grade, through this only last number will get update
+    }
+  }
 }
+provider "azurerm" {
+features{}
+}
+
 variable "prefix"{
   default = "rahul"
 }
 
-data "azurerm_resource_group" "example" {
+data "azurerm_resource_group" "${var.prefix}-rg" {
   name = "Devops_Kochi"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
+resource "azurerm_virtual_network" "rahulvnet" {
+  name                = "rahulvnet-1"
+  address_space       = ["10.0.0.0/16"] #this is a list item
+  location            = data.azurerm_resource_group.rahul-rg.location
+  resource_group_name = data.azurerm_resource_group.rahul-rg.name
+  tags                ={ 
+                        "Name" = "rahulvnet-1"
+                        }
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
-  resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+resource "azurerm_subnet" "rahulsubnet" {
+  name                 = "rahulsubnet-1"
+  resource_group_name  = data.azurerm_resource_group.rahul-rg.name
+  virtual_network_name = azurerm_virtual_network.rahulvnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
+resource "azurerm_network_interface" "rahul-nic" {
+  name                = "rahul-nic"
+  location            = data.azurerm_resource_group.rahul-rg.location
+  resource_group_name = data.azurerm_resource_group.rahul-rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.rahulsubnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "example-machine"
-  resource_group_name = data.azurerm_resource_group.example.name
-  location            = data.azurerm_resource_group.example.location
+resource "azurerm_windows_virtual_machine" "rahulvm" {
+  name                = "rahul-machine"
+  resource_group_name = data.azurerm_resource_group.rahul-rg.name
+  location            = data.azurerm_resource_group.rahul-rg.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
   admin_password      = "P@$$w0rd1234!"
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.rahul-nic.id,
   ]
 
   os_disk {
