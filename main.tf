92e7e9deeca93415cfc76ce7fd41df11c5c72adf
rahulreghunath-tcs <92011839+rahulreghunath-tcs@users.noreply.github.com>
2021-10-11 19:30:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,14 +27,12 @@ resource "azurerm_subnet" "rahulsubnet" {
   resource_group_name  = data.azurerm_resource_group.rahulrg.name
   virtual_network_name = azurerm_virtual_network.rahulvnet.name
   address_prefixes     = ["10.0.2.0/24"]
-   tags                ={ 
-                        "env" = "rahuldev"
-                        }
+ 
 }
 resource "azurerm_public_ip" "rahulpublicip" {
   name                = "rahulPublicIp1"
-  resource_group_name = azurerm_resource_group.rahulrg.name
-  location            = azurerm_resource_group.rahulrg.location
+  resource_group_name = data.azurerm_resource_group.rahulrg.name
+  location            = data.azurerm_resource_group.rahulrg.location
   allocation_method   = "Static"
 
   tags = {
