1989fd72ed60e0c2bd9fcb4ea6a6a7f81c76c24e
rahulreghunath-tcs <92011839+rahulreghunath-tcs@users.noreply.github.com>
2021-10-11 18:55:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">=2.80.0"
-      # version = "~>2.80.0" For production grade, through this only last number will get update
+      version = ">= 2.0" 
     }
   }
 }
