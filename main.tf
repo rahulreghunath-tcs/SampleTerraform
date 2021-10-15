e2ce7cc7f4f68487fec21d48de1b89c1c68ecbe6
rahulreghunath-tcs <92011839+rahulreghunath-tcs@users.noreply.github.com>
2021-10-15 09:43:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   backend "azurerm" {
     resource_group_name  = "Devops_Kochi"
@@ -14,82 +13,39 @@ provider "azurerm" {
   skip_provider_registration = true
   features {}
 }
-variable "prefix"{
-  default = "rahul"
-}
+
 
 data "azurerm_resource_group" "rahulrg" {
   name = "Devops_Kochi"
 }
 
-resource "azurerm_virtual_network" "rahulvnet" {
-  name                = "rahulvnet-1"
-  address_space       = ["10.0.0.0/16"] #this is a list item
-  location            = data.azurerm_resource_group.rahulrg.location
-  resource_group_name = data.azurerm_resource_group.rahulrg.name
-  tags                ={ 
-                        "env" = "rahuldev"
-                        }
-}
+resource "azurerm_app_service_plan" "rahulappserviceplan" {
+  name                = "rahul-appserviceplan"
+  location            = azurerm_resource_group.rahulrg.location
+  resource_group_name = azurerm_resource_group.rahulrg.name
 
-resource "azurerm_subnet" "rahulsubnet" {
-  name                 = "rahulsubnet-1"
-  resource_group_name  = data.azurerm_resource_group.rahulrg.name
-  virtual_network_name = azurerm_virtual_network.rahulvnet.name
-  address_prefixes     = ["10.0.2.0/24"]
- 
-}
-resource "azurerm_public_ip" "rahulpublicip" {
-  name                = "rahulPublicIp1"
-  resource_group_name = data.azurerm_resource_group.rahulrg.name
-  location            = data.azurerm_resource_group.rahulrg.location
-  allocation_method   = "Static"
-
-  tags = {
-    env = "dev"
+  sku {
+    tier = "Standard"
+    size = "S1"
   }
 }
+resource "azurerm_app_service" "rahulappservice" {
+  name                = "rahul-app-service"
+  location            = azurerm_resource_group.rahulrg.location
+  resource_group_name = azurerm_resource_group.rahulrg.name
+  app_service_plan_id = azurerm_app_service_plan.rahulappserviceplan.id
 
-resource "azurerm_network_interface" "rahul-nic" {
-  name                = "rahul-nic"
-  location            = data.azurerm_resource_group.rahulrg.location
-  resource_group_name = data.azurerm_resource_group.rahulrg.name
-   tags                ={ 
-                        "env" = "rahuldev"
-                        }
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.rahulsubnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id         =azurerm_public_ip.rahulpublicip.id
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
   }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+
 }
 
-resource "azurerm_windows_virtual_machine" "rahulvm" {
-  name                = "rahulmachine"
-  resource_group_name = data.azurerm_resource_group.rahulrg.name
-  location            = data.azurerm_resource_group.rahulrg.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  admin_password      = "P@$$w0rd1234!"
-  network_interface_ids = [
-    azurerm_network_interface.rahul-nic.id,
-  ]
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
 
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
-  tags                ={ 
-                        "env" = "rahuldev"
-                        }
-  
-}
